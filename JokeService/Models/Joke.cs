037287c34efd9084a18a935a5c8692b0a511e187
Bjørn Göttler <bjoern@goettler.dk>
2024-10-04 21:56:46 +0200

    :sparkles: Add model and context

--- JokeService/Models/Joke.cs
+++ JokeService/Models/Joke.cs
@@ -1,6 +1,12 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 namespace JokeService.Models;
 
 public class Joke
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public string Content { get; set; } = string.Empty;
     
 }
